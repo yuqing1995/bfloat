0c4f142739fba348c78f7db844133707801e63f0
yuqing1995 <43278140+yuqing1995@users.noreply.github.com>
2019-10-25 01:28:26 -0400

    first try

---
+++ test_bfloat.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "bfloat16.h"
+
+using namespace std;
+
+int main()
+{
+   cout<<"BFloat16 Calculation\n";
+   // Test binary2float
+   Bfloat16 bfloat1(-8.1852e-34); // -8.1852e-34
+   Bfloat16 bfloat2(-1.25185e-33); // -1.25185e-33
+   Bfloat16 bfloat3(-1.0625); // -1.0625
+   Bfloat16 bfloat4(1.625); // 1.625
+   cout<<"binary2float:\nap_float1: " << bfloat1.binary2float() << endl;
+   cout<<"ap_float2: " << bfloat2.binary2float() << endl;
+   cout<<"ap_float3: " << bfloat3.binary2float() << endl;
+   cout<<"ap_float4: " << bfloat4.binary2float() << endl;
+   
+   
+   return 0;
+}
